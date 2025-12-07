9f307e8a1f9d00d1e586cd7c619dafbe94290a75
Fralopala2 <spanocrypt@gmail.com>
2025-12-07 16:41:53 +0100

    fix: simplificar configuracion gradle y agregar compileSdk en propiedades

--- medusse_app/android/build.gradle.kts
+++ medusse_app/android/build.gradle.kts
@@ -5,23 +5,11 @@ allprojects {
     }
 }
 
-// Aplicar plugins sin aplicarlos en el root
 plugins {
     id("com.android.application") apply false
     id("com.android.library") apply false
     id("dev.flutter.flutter-gradle-plugin") apply false
     id("kotlin-android") apply false
-}
-
-// Configuración común para subproyectos Android
-subprojects {
-    afterEvaluate {
-        if (plugins.hasPlugin("com.android.library")) {
-            android {
-                compileSdk = 35
-            }
-        }
-    }
 }
 
 val newBuildDir: Directory =
